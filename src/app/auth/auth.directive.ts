5b3737146b159d64d1090ebf4bc7838f8b57e214
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-30 12:42:08 +0200

    used structural directive

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, inject, input, effect } from '@angular/core';
+import {
+  Directive,
+  inject,
+  input,
+  effect,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 @Directive({
@@ -8,12 +15,16 @@ import { AuthService } from './auth.service';
 export class AuthDirective {
   userType = input.required<Permission>({ alias: 'appAuth' });
   private authService = inject(AuthService);
+  private templateRef = inject(TemplateRef);
+  private viewContainerRef = inject(ViewContainerRef);
   constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userType()) {
         console.log('SHOW ELEMENT');
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
         console.log('DO NO SHOW ELEMENT');
+        this.viewContainerRef.clear();
       }
     });
   }
